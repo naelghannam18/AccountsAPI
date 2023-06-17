6b6907d83d072bdfb3275aec71bdf07d412be80d
nael ghannam <nzg00001@students.aust.edu.lb>
2023-06-17 23:22:19 +0300

    Modified Customer Service Unit Tests

--- core.tests/CustomerServiceTests.cs
+++ core.tests/CustomerServiceTests.cs
@@ -2,6 +2,7 @@
 using Core.Services.Implementations;
 using Domain.DTOs;
 using Domain.Models;
+using FluentAssertions;
 using Infrastructure.Repositories;
 using Moq;
 using System.Net;
@@ -9,6 +10,12 @@ using Xunit;
 
 namespace Core.Tests;
 
+/// <summary>
+/// This is a sample Unit test for Customer Service
+/// I Have little knowledge of Unit Tests 
+/// I did a brief research in respect to the project deadline
+/// This class showcases my learning outcomes of Unit testing in .Net
+/// </summary>
 public class CustomerServiceTests
 {
     private readonly Mock<IGenericRepository<Customer>> Repository;
@@ -31,24 +38,27 @@ public class CustomerServiceTests
             new Customer { Id = 1, Name = "John", Surname = "Doe" },
             new Customer { Id = 2, Name = "Jane", Surname = "Smith" }
         };
+
         var customerDTOs = customers.Select(c => new CustomerDTO { Id = c.Id, Name = c.Name, Surname = c.Surname }).ToList();
+
         Repository.Setup(r => r.GetAll()).ReturnsAsync(customers.AsQueryable());
-        Mapper.Setup(m => m.Map<CustomerDTO>(It.IsAny<Customer>())).Returns((Customer c) =>
-            new CustomerDTO { Id = c.Id, Name = c.Name, Surname = c.Surname });
+
+        Mapper.Setup(m => m.Map<CustomerDTO>(It.IsAny<Customer>()))
+            .Returns((Customer c) => new CustomerDTO
+            { Id = c.Id, Name = c.Name, Surname = c.Surname });
 
         // Act
         var response = await CustomerService.GetAllCustomers();
 
         // Assert
-        Assert.Equal(HttpStatusCode.OK, response.Status);
-        Assert.True(customerDTOs.Count == response.Data.Count);
+
+        response.Status.Should().Be(HttpStatusCode.OK);
+        customerDTOs.Count.Should().Be(response.Data.Count);
+        response.Data.Should().BeOfType(typeof(List<CustomerDTO>));
 
         for (int i = 0; i < customerDTOs.Count; i++)
         {
-            Assert.Equal(customerDTOs[i].Id, response.Data[i].Id);
-            Assert.Equal(customerDTOs[i].Name, response.Data[i].Name);
-            Assert.Equal(customerDTOs[i].Surname, response.Data[i].Surname);
-            Assert.Equal(customerDTOs[i].Account, response.Data[i].Account);
+            customerDTOs[i].Should().BeEquivalentTo(response.Data[i]);
         }
     }
 
@@ -62,13 +72,15 @@ public class CustomerServiceTests
             new Customer { Id = 1, Name = "John", Surname = "Doe" }
         };
 
-        var customerDTOs = new CustomerDTO()
+        var customerDTO = new CustomerDTO()
         {
             Name = customer[0].Name,
             Surname = customer[0].Surname,
             Id = customer[0].Id,
         };
+
         Repository.Setup(r => r.GetById(1)).ReturnsAsync(customer.AsQueryable());
+
         Mapper.Setup(m => m.Map<CustomerDTO>(It.IsAny<Customer>()))
             .Returns((Customer c) => new CustomerDTO { Id = c.Id, Name = c.Name, Surname = c.Surname });
 
@@ -76,27 +88,42 @@ public class CustomerServiceTests
         var response = await CustomerService.GetCustomerById(1);
 
         // Assert
-        Assert.Equal(HttpStatusCode.OK, response.Status);
-        Assert.Equal(customer[0].Id, response.Data.Id);
-        Assert.Equal(customer[0].Name, response.Data.Name);
-        Assert.Equal(customer[0].Surname, response.Data.Surname);
+        response.Status.Should().Be(HttpStatusCode.OK);
+        response.Data.Should().BeEquivalentTo(customerDTO);
     }
 
     [Fact]
     public async Task CreateCustomer_ShouldReturnId()
     {
+        var createCustomerDto = new CreateCustomerDTO()
+        {
+            Name = "Name",
+            Surname = "Surname"
+        };
         // Arrange
         var customer = new Customer()
         {
             Id = 1,
             Name = "Name",
-            Surname = "Surname"
+            Surname = "Surname",
+            Account = new()
+            {
+                Balance = 100,
+                CreatedDate = DateTime.UtcNow,
+                CustomerId = 1,
+                Id = 1,
+                IsRemoved = false,
+                ReceivedTransactions = new List<Transaction>(),
+                SentTransactions = new List<Transaction>(),
+            }
         };
-
-        Repository.Setup()
+        Repository.Setup(r => r.Create(It.IsAny<Customer>())).ReturnsAsync(customer);
 
         // Act
+        var response = await CustomerService.CreateCustomer(createCustomerDto);
 
         // Assert
+        response.Status.Should().Be(HttpStatusCode.Created);
+        response.Data.Should().BeGreaterThan(0);
     }
 }
